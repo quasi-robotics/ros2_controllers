166c809039d81ea88eef0598b95656e2cac0b01c
Fedor Bokov <fedor@quasi.ai>
2023-04-14 11:32:04 -0400

    log info about tolerances violation

--- joint_trajectory_controller/src/joint_trajectory_controller.cpp
+++ joint_trajectory_controller/src/joint_trajectory_controller.cpp
@@ -211,7 +211,7 @@ controller_interface::return_type JointTrajectoryController::update(
         if (
           (before_last_point || first_sample) &&
           !check_state_tolerance_per_joint(
-            state_error_, index, default_tolerances_.state_tolerance[index], false))
+            state_error_, index, default_tolerances_.state_tolerance[index], true))
         {
           tolerance_violated_while_moving = true;
         }
@@ -219,7 +219,7 @@ controller_interface::return_type JointTrajectoryController::update(
         if (
           !before_last_point &&
           !check_state_tolerance_per_joint(
-            state_error_, index, default_tolerances_.goal_state_tolerance[index], false))
+            state_error_, index, default_tolerances_.goal_state_tolerance[index], true))
         {
           outside_goal_tolerance = true;
 
